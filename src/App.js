98caf62bcbb96784fc97e3ab942785e266f4496a
Dan Seethaler <dan@seethalers.com>
2018-06-19 18:15:43 -0400

    Add base selector

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import {ThemeProvider} from 'emotion-theming';
-import React, {Component} from 'react';
-import './App.css';
-import theme from './config/theme';
-import setupSchedule from './services/schedule';
-import Container from './components/Container';
-import NavBar from './components/NavBar';
+import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
+import './App.css';
+import BaseSelector from './components/BaseSelector';
+import theme from './config/theme';
 
-class App extends Component {
-  state = {loaded: false};
-
-  componentDidMount() {
-    setupSchedule().then(() => {
-      this.setState({loaded: true});
-    });
-  }
-  render() {
-    if (!this.state.loaded) return null;
-
-    return (
-      <Router>
-        <ThemeProvider theme={theme}>
-          <React.Fragment>
-            <Container />
-            <NavBar />
-          </React.Fragment>
-        </ThemeProvider>
-      </Router>
-    );
-  }
-}
-
-export default App;
+export default () => (
+  <Router>
+    <ThemeProvider theme={theme}>
+      <BaseSelector />
+    </ThemeProvider>
+  </Router>
+);
---
+++ src/components/BaseSelector.jsx
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+import {setupAirtable} from '../airtable';
+import bases from '../config/bases';
+import setupSchedule from '../services/schedule';
+import Container from './Container';
+
+const SelectBase = () => (
+  <div>
+    {Object.keys(bases).map(base => {
+      const {baseKey, title} = bases[base];
+
+      return (
+        <div key={baseKey}>
+          <Link to={`/${base}`}>{title}</Link>
+        </div>
+      );
+    })}
+  </div>
+);
+
+let currentBaseName = '';
+
+class BaseSelector extends Component {
+  state = {loaded: false};
+
+  componentDidMount() {
+    this.checkAndInitialize();
+    this.unlisten = this.props.history.listen(this.checkAndInitialize);
+  }
+
+  componentWillUnmount() {
+    this.unlisten();
+  }
+
+  getBaseFromPath = () => {
+    const {pathname} = this.props.history.location;
+    return pathname.split('/')[1];
+  };
+
+  checkAndInitialize = () => {
+    const baseName = this.getBaseFromPath();
+
+    if (!bases[baseName]) return;
+
+    if (baseName !== currentBaseName) {
+      this.initialize(baseName);
+    }
+    currentBaseName = baseName;
+  };
+
+  initialize = baseName => {
+    setupAirtable(baseName);
+    this.setState({loaded: false});
+
+    setupSchedule().then(() => {
+      this.setState({loaded: true});
+    });
+  };
+
+  render() {
+    const baseName = this.getBaseFromPath();
+
+    if (!bases[baseName]) {
+      return <SelectBase />;
+    }
+
+    if (!this.state.loaded) return <Container loading />;
+
+    return <Container />;
+  }
+}
+
+export default withRouter(BaseSelector);

c2bd6c7464517a3b156f2f23397f1558921c0c33
Dan Seethaler <dan@seethalers.com>
2018-06-20 08:38:19 -0400

    Add redirect for missing routes

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Redirect, Route, Switch} from 'react-router-dom';
 import Home from '../pages/Home';
 import Hymns from '../pages/Hymns';
 import Links from '../pages/Links';
@@ -17,8 +17,15 @@ export default ({loading, title, children}) => {
       <Switch>
         <Route exact path="/:location" component={Home} />
         <Route exact path="/:location/hymns" component={Hymns} />
-        <Route exact path="/:location/links" component={Links} />
+        <Route exact path="/:location/info" component={Info} />
         <Route exact path="/:location/schedule" component={Schedule} />
+        <Route
+          render={({location}) => {
+            const baseName = location.pathname.split('/')[1];
+
+            return <Redirect to={{pathname: `/${baseName}`}} />;
+          }}
+        />
       </Switch>
 
       <NavBar />
